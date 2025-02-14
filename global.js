bd8102ebcbc0d2737ff329dbb586c8746fec404d
boruilin <b6lin@ucsd.edu>
2025-02-13 23:25:13 -0800

    lab 6 step 0

--- global.js
+++ global.js
@@ -9,6 +9,7 @@ let pages = [
   { url: 'projects/index.html', title: 'Projects' },
   { url: 'contact/index.html', title: 'Contact' },
   { url: 'resume/index.html', title: 'Resume' },
+  { url: 'meta/index.html', title: 'Meta' },
   { url: 'https://github.com/boruilin', title: 'Profile' },
 ];
 
